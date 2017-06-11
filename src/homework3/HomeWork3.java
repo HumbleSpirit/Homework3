134c424d456f1be053b387849610724c2c6a4d28
HumbleSpirit <borodkin@gmail.com>
2017-06-11 19:46:35 +0300

    8th task

--- src/homework3/HomeWork3.java
+++ src/homework3/HomeWork3.java
@@ -30,18 +30,24 @@ public class HomeWork3 {
     public static void main(String[] args) throws InterruptedException {
         int firstValue = 0;
         int secondValue = 0;
-        int tempValue1 = 0;
-        int tempValue2 = 0;
-        int tempValue3 = 0;
-        int tempValue4 = 0;
+        double tempValue1 = 0;
+        double tempValue2 = 0;
+        double tempValue3 = 0;
+        double tempValue4 = 0;
         int result1 = 0;
         int result2 = 0;
         double percentValue = 0;
         double accountValue = 0;
         double bankMonthly1 = 0;
+        int a = 0;
+        int temp1 = 0;
+        int temp2 = 0;        
+        int temp3 = 0;        
+        int temp4 = 0;
+        int temp5 = 0;
         
         Scanner in = new Scanner(System.in); 
-        System.out.println("Task 1");
+        /*System.out.println("Task 1");
         System.out.println("Move middle digits in two three-digits numbers");
         System.out.println("Please input three digits of first value");  
         firstValue = in.nextInt();  
@@ -81,6 +87,58 @@ public class HomeWork3 {
         System.out.println("Monthly percents amount is " + bankMonthly1);  
         drawStars();
                        
+        System.out.println("Task 5");
+        System.out.println("How many items could buy for specified amount, and how many change is remain");
+        System.out.println("Please input amount of available money");  
+        accountValue = in.nextDouble();  
+        System.out.println("Please input cost of one item");  
+        tempValue1 = in.nextDouble();  
+        tempValue2 = (accountValue / tempValue1);
+        System.out.println("You can buy " + (int)tempValue2 + " items");  
+        tempValue3 = (accountValue -  (tempValue2 * tempValue1));
+        System.out.println("You have remain " + tempValue3);  
+        drawStars();
+        
+        System.out.println("Task 6");
+        System.out.println("Pound to kilograms converter");
+        final double pound = 0.45359237;   //lb.av
+        double pounds_in_kilo = 0;
+        System.out.println("Please enter weight in kilograms");
+        pounds_in_kilo = in.nextDouble(); 
+        tempValue1 = pounds_in_kilo / pound;
+        System.out.println(pounds_in_kilo + " kilograms contains " + (float)tempValue1 + " pounds");
+        drawStars();
+        
+        double tempValue5 = 0;
+        System.out.println("Task 7");
+        System.out.println("Let's count, how much paint we need to paint a room");
+        System.out.println("Please input height of room");  
+        tempValue1 = in.nextDouble();  
+        System.out.println("Please input length of room");  
+        tempValue2 = in.nextDouble();  
+        System.out.println("Please input width of room");  
+        tempValue3 = in.nextDouble();  
+        tempValue4 = (2 * (tempValue2 * tempValue3) + (tempValue2 * tempValue1) + (tempValue1 * tempValue3));
+        System.out.println("Total square of room is " + tempValue4 + " square meters");
+        System.out.println("Total square of room, which should be painted is " + ((tempValue4 - (tempValue4 /100) * 20)) + " square meters");
+        tempValue5 = (tempValue4 - (tempValue4 /100) * 20) * 0.5;
+        System.out.println("We need " + tempValue5 + " liters of paint to paint " + ((tempValue4 - (tempValue4 /100) * 20)) + " square meters of room");
+        drawStars();
+        
+         
+        System.out.println("Task 8");        
+        System.out.println("Reverce 3-digits number");
+        System.out.println("Please enter number");
+        a = in.nextInt();  
+        temp1 = a / 100; // Сотни первого числа
+        temp2 = a % 100 / 10; // Десятки первого числа
+        temp3 = a % 10; //Единицы первого числа
+        System.out.println((temp3 * 100) + (temp2 * 10) + temp1);
+        drawStars();*/
+
+        
+
+        
     }
     
 }
